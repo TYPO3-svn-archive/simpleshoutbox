64cb5de9d9cc15817d9b8c33b4aaea8b244c2847
peschuster <peschuster@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-10-20 21:53:03 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/simpleshoutbox/trunk@13190 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.sql
+++ ext_tables.sql
@@ -3,15 +3,15 @@
 #
 CREATE TABLE tx_simpleshoutbox_messages (
 	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-	tstamp int(11) DEFAULT '0' NOT NULL,
-	crdate int(11) DEFAULT '0' NOT NULL,
-	deleted tinyint(4) DEFAULT '0' NOT NULL,
-	userid tinyint(4) DEFAULT '0' NOT NULL,
-	name tinytext DEFAULT '' NOT NULL,
-	message tinytext DEFAULT '' NOT NULL,
-	doublecheck tinytext DEFAULT '' NOT NULL,
-	
-	PRIMARY KEY (uid),
+	pid int(11) NOT NULL default '0',
+	tstamp int(11) NOT NULL default '0',
+	crdate int(11) NOT NULL default '0',
+	deleted tinyint(4) NOT NULL default '0',
+	userid tinyint(4) default '0',
+	`name` tinytext,
+	message tinytext,
+	doublecheck tinytext,
+
+	PRIMARY KEY  (uid),
 	KEY parent (pid)
 );
