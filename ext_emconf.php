1f8d8f7dfa14ab320f1d33d61551c01842b0fc90
peschuster <peschuster@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-03-17 12:01:13 +0000

    released version 0.2.0 in TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/simpleshoutbox/trunk@17958 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "simpleshoutbox"
 #
-# Auto generated 12-03-2009 01:01
+# Auto generated 17-03-2009 12:57
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
-	'state' => 'alpha',
+	'state' => 'beta',
 	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.1.0',
+	'version' => '0.2.0',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 			'smilie' => '',
 		),
 	),
-	'_md5_values_when_last_written' => 'a:18:{s:9:"ChangeLog";s:4:"d6c2";s:32:"class.tx_simpleshoutbox_ajax.php";s:4:"efc9";s:31:"class.tx_simpleshoutbox_api.php";s:4:"562b";s:12:"ext_icon.gif";s:4:"0803";s:17:"ext_localconf.php";s:4:"08ae";s:14:"ext_tables.php";s:4:"54b3";s:14:"ext_tables.sql";s:4:"3cca";s:35:"icon_tx_simpleshoutbox_messages.gif";s:4:"475a";s:16:"locallang_db.xml";s:4:"50a7";s:22:"res/simpleshoutbox.css";s:4:"59c5";s:21:"res/simpleshoutbox.js";s:4:"2a1e";s:17:"res/template.html";s:4:"6290";s:40:"hooks/class.tx_simpleshoutbox_smilie.php";s:4:"2565";s:35:"pi1/class.tx_simpleshoutbox_pi1.php";s:4:"6419";s:17:"pi1/locallang.xml";s:4:"ee74";s:24:"pi1/static/editorcfg.txt";s:4:"094d";s:20:"pi1/static/setup.txt";s:4:"6d25";s:14:"doc/manual.sxw";s:4:"8f0a";}',
+	'_md5_values_when_last_written' => 'a:18:{s:9:"ChangeLog";s:4:"015c";s:32:"class.tx_simpleshoutbox_ajax.php";s:4:"dd51";s:31:"class.tx_simpleshoutbox_api.php";s:4:"8511";s:12:"ext_icon.gif";s:4:"0803";s:17:"ext_localconf.php";s:4:"08ae";s:14:"ext_tables.php";s:4:"54b3";s:14:"ext_tables.sql";s:4:"3cca";s:35:"icon_tx_simpleshoutbox_messages.gif";s:4:"475a";s:16:"locallang_db.xml";s:4:"50a7";s:22:"res/simpleshoutbox.css";s:4:"59c5";s:21:"res/simpleshoutbox.js";s:4:"2a1e";s:17:"res/template.html";s:4:"6290";s:40:"hooks/class.tx_simpleshoutbox_smilie.php";s:4:"54ee";s:35:"pi1/class.tx_simpleshoutbox_pi1.php";s:4:"5cea";s:17:"pi1/locallang.xml";s:4:"ee74";s:24:"pi1/static/editorcfg.txt";s:4:"094d";s:20:"pi1/static/setup.txt";s:4:"6d25";s:14:"doc/manual.sxw";s:4:"62fb";}',
 	'suggests' => array(
 	),
 );
